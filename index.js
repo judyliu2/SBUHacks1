8e723c876f6e2761ed91592629c12840f16cbe33
NobleTan <noblet@sfu.ca>
2020-09-26 02:35:08 -0700

    home screen complete + instructions

---
+++ index.js
@@ -0,0 +1,10 @@
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+function clickinner() {
+  location.href = 'play.html';
+}
